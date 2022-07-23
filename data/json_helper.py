44457da71eef8bb22c77294eb34f20257f297a10
Kendra <kendra@Zipcoders-MacBook-Pro-45.local>
2022-07-23 12:55:47 -0400

    part b

--- data/json_helper.py
+++ data/json_helper.py
@@ -1,7 +1,20 @@
-import os
-import json
+import os, json, pickle
+
+
 
 def read_json(path):
     open_json = open(path)
     json_object = json.load(open_json)
     return json_object
+
+def read_all_json_files(path):
+    all_json = []
+    for files in os.walk(path):
+        for file in files:
+            if file.endswith('.json'):
+                result = read_json(os.path.join(path, file))
+                all_json.append(result)
+    return all_json
+
+
+
